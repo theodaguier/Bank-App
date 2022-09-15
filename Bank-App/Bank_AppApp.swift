c5469c7351d0f684d936b4007ee4e5b7a7051412
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-09-15 10:41:55 +0200

    Initial Commit

---
+++ Bank-App/Bank_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_AppApp.swift
+//  Bank-App
+//
+//  Created by Théo Daguier on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bank_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
